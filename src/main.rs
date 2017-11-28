4f4fdc7e540c45678e21491e1647054eb7ec2844
Anthony D'Ambrosio <nsa.anthony@gmail.com>
2017-11-28 02:13:14 -0800

    can input multiple files & can skip with empty newline (ie: press enter)

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ extern crate clap;
 
 use clap::{App, Arg};
 use std::process::Command;
+use std::io;
 
 fn main() {
     //let mut rng = rand::thread_rng();
@@ -15,20 +16,34 @@ fn main() {
              .value_name("VOLUME"))
         .arg(Arg::with_name("INPUT")
                 .required(true)
-                .index(1))
+                .multiple(true))
         .get_matches();
 
-    let music_file = matches.value_of("INPUT").unwrap();
+    let music_files: Vec<&str> = matches.values_of("INPUT").unwrap().collect();
     let volume_arg = value_t!(matches, "volume", u32).unwrap_or(5);
     let volume_str = &(volume_arg as f32 / 100.0).to_string();
 
-    let ecode = Command::new("afplay")
-        .arg(music_file)
-        .args(&["-v", volume_str])
-        .spawn()
-        .expect("failed to execute afplay")
-        .wait()
-        .expect("failed to wait for afplay");
+    //TODO: make music_files an infinite iterator over its shuffle
+    for file in music_files {
+        println!("Playing {}", file);
+        let mut child = Command::new("afplay")
+            .arg(file)
+            .args(&["-v", volume_str])
+            .spawn()
+            .expect("failed to execute afplay");
 
-    assert!(ecode.success());
+        while {
+            let exit = child.try_wait();
+            if exit.is_ok() { exit.unwrap().is_none() } else { true }
+        } {
+            let mut input = String::new();
+            io::stdin().read_line(&mut input).expect("failed to get some input");
+            if input == "\n" {
+                child.kill().expect("was no afplay?");
+                child.wait().unwrap();
+            }
+        }
+    }
+
+    println!("K THX BAI!");
 }
