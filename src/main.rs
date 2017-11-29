c885e03930999928cf32142d225c9ab78eaead97
Anthony D'Ambrosio <nsa.anthony@gmail.com>
2017-11-28 20:43:09 -0800

    Shuffles the specified songs, uses a bag shuffle with no repeats

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,74 @@
-extern crate rand;
 #[macro_use]
 extern crate clap;
+extern crate itertools;
+extern crate rand;
 
 use clap::{App, Arg};
+use rand::{Rng};
+
 use std::process::Command;
 use std::io;
 
-fn main() {
-    //let mut rng = rand::thread_rng();
+fn shuffle<T>(coll: &mut Vec<T>) {
+    let mut rng = rand::thread_rng();
+    rng.shuffle(coll);
+}
 
+struct TetrisShuffle<T> {
+    coll: Vec<T>,
+    len: usize,
+    idx: usize,
+    last: Option<T>
+}
+
+impl<T> TetrisShuffle<T> {
+    fn new(mut coll: Vec<T>) -> TetrisShuffle<T> {
+        let len = coll.len();
+        shuffle(&mut coll);
+        TetrisShuffle { coll: coll, len: len, idx: 0, last: None}
+    }
+}
+
+impl<T: Clone + Copy + Eq> Iterator for TetrisShuffle<T> {
+    type Item = T;
+    fn next(&mut self) -> Option<Self::Item> {
+        let next_item = self.coll[self.idx].clone();
+        if self.idx == self.len-1 {
+            self.last = Some(next_item);
+            self.idx = 0;
+            shuffle(&mut self.coll);
+            while self.coll[0] == self.last.unwrap() {
+                shuffle(&mut self.coll);
+            }
+        } else {
+            self.idx += 1;
+        }
+        Some(next_item)
+    }
+}
+
+fn tetris_shuffle<T>(coll: Vec<T>) -> TetrisShuffle<T> {
+    TetrisShuffle::new(coll)
+}
+
+#[cfg(test)]
+mod tests {
+    use tetris_shuffle;
+    use itertools::Itertools;
+    #[test]
+    fn tetris_shuffle_works() {
+        let to_shuffle = vec![1,2,3,4,5];
+        let shuffled: Vec<usize> =
+            tetris_shuffle(to_shuffle)
+            .chunks(5).into_iter()
+            .take(3)
+            .map(|xs| xs.sum())
+            .collect();
+        assert_eq!(shuffled, [15, 15, 15], "grouped in groups of the original collection length")
+    }
+}
+
+fn main() {
     let matches = App::new("player-rs")
         .version(crate_version!())
         .arg(Arg::with_name("volume")
@@ -23,8 +83,7 @@ fn main() {
     let volume_arg = value_t!(matches, "volume", u32).unwrap_or(5);
     let volume_str = &(volume_arg as f32 / 100.0).to_string();
 
-    //TODO: make music_files an infinite iterator over its shuffle
-    for file in music_files {
+    for file in tetris_shuffle(music_files) {
         println!("Playing {}", file);
         let mut child = Command::new("afplay")
             .arg(file)
