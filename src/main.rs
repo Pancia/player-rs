16dd98c3bfcbb61cfa8b5bcabaa1b868fe2fa649
Anthony D'Ambrosio <nsa.anthony@gmail.com>
2017-11-30 13:56:11 -0800

    can now expand (once) INPUT dirs

--- src/main.rs
+++ src/main.rs
@@ -8,6 +8,8 @@ use rand::{Rng};
 
 use std::process::Command;
 use std::io;
+use std::fs;
+use std::path::{Path, PathBuf};
 
 fn shuffle<T>(coll: &mut Vec<T>) {
     let mut rng = rand::thread_rng();
@@ -29,21 +31,21 @@ impl<T> TetrisShuffle<T> {
     }
 }
 
-impl<T: Clone + Copy + Eq> Iterator for TetrisShuffle<T> {
+impl<T: Clone + Eq> Iterator for TetrisShuffle<T> {
     type Item = T;
     fn next(&mut self) -> Option<Self::Item> {
         let next_item = self.coll[self.idx].clone();
         if self.idx == self.len-1 {
-            self.last = Some(next_item);
+            self.last = Some(next_item.clone());
             self.idx = 0;
             shuffle(&mut self.coll);
-            while self.coll[0] == self.last.unwrap() {
+            while self.coll[0] == next_item {
                 shuffle(&mut self.coll);
             }
         } else {
             self.idx += 1;
         }
-        Some(next_item)
+        Some(next_item.clone())
     }
 }
 
@@ -68,6 +70,15 @@ mod tests {
     }
 }
 
+fn expand_dirs(path: &Path) -> Vec<PathBuf> {
+    if fs::metadata(path).unwrap().is_dir() {
+        //clone or use PathBuf
+        fs::read_dir(path).unwrap().map(|r| r.unwrap().path()).collect()
+    } else {
+        vec![path.to_path_buf()]
+    }
+}
+
 fn main() {
     let matches = App::new("player-rs")
         .version(crate_version!())
@@ -79,12 +90,15 @@ fn main() {
                 .multiple(true))
         .get_matches();
 
-    let music_files: Vec<&str> = matches.values_of("INPUT").unwrap().collect();
+    let input_paths: Vec<&str> = matches.values_of("INPUT").unwrap().collect();
+    let music_files = input_paths.iter().map(|s| Path::new(s))
+        .flat_map(expand_dirs).collect();
+
     let volume_arg = value_t!(matches, "volume", u32).unwrap_or(5);
     let volume_str = &(volume_arg as f32 / 100.0).to_string();
 
     for file in tetris_shuffle(music_files) {
-        println!("Playing {}", file);
+        println!("Playing {}", file.display());
         let mut child = Command::new("afplay")
             .arg(file)
             .args(&["-v", volume_str])
