81b0523b1771ccd0334413d1861f9efa63dcdef4
Anthony D'Ambrosio <nsa.anthony@gmail.com>
2017-12-01 05:19:20 -0800

    - fixed single input not playing anything (would infinitely shuffle) - fixed stdin reading to use a producer thread (ie: can skip again now) - rustfmt works... nice!

--- src/main.rs
+++ src/main.rs
@@ -10,12 +10,15 @@ use std::process::Command;
 use std::io;
 use std::fs;
 use std::path::{Path, PathBuf};
+use std::sync::mpsc;
+use std::thread;
 
 fn shuffle<T>(coll: &mut Vec<T>) {
     let mut rng = rand::thread_rng();
     rng.shuffle(coll);
 }
 
+#[derive(Debug)]
 struct TetrisShuffle<T> {
     coll: Vec<T>,
     len: usize,
@@ -38,7 +41,9 @@ impl<T: Clone + Eq> Iterator for TetrisShuffle<T> {
     type Item = T;
     fn next(&mut self) -> Option<Self::Item> {
         let next_item = self.coll[self.idx].clone();
-        if self.idx == self.len - 1 {
+        if self.len == 1 {
+            return Some(next_item);
+        } else if self.idx == self.len - 1 {
             self.idx = 0;
             shuffle(&mut self.coll);
             while self.coll[0] == next_item {
@@ -59,6 +64,7 @@ fn tetris_shuffle<T>(coll: Vec<T>) -> TetrisShuffle<T> {
 mod tests {
     use tetris_shuffle;
     use itertools::Itertools;
+
     #[test]
     fn tetris_shuffle_works() {
         let to_shuffle = vec![1, 2, 3, 4, 5];
@@ -96,7 +102,7 @@ fn main() {
         .get_matches();
 
     let input_paths: Vec<&str> = matches.values_of("INPUT").unwrap().collect();
-    let music_files = input_paths
+    let music_files: Vec<PathBuf> = input_paths
         .iter()
         .map(|s| Path::new(s))
         .flat_map(expand_dirs)
@@ -105,32 +111,30 @@ fn main() {
     let volume_arg = value_t!(matches, "volume", u32).unwrap_or(5);
     let volume_str = &(volume_arg as f32 / 100.0).to_string();
 
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || loop {
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("stdin");
+        match input {
+            _ => tx.send("kill").unwrap(),
+        }
+    });
+
     for file in tetris_shuffle(music_files) {
         println!("Playing {}", file.display());
+
         let mut child = Command::new("afplay")
             .arg(file)
             .args(&["-v", volume_str])
             .spawn()
             .expect("failed to execute afplay");
 
-        while {
-            let exit = child.try_wait();
-            //TODO use map_ok ?
-            if exit.is_ok() {
-                exit.unwrap().is_none()
-            } else {
-                true
-            }
-        }
-        {
-            let mut input = String::new();
-            io::stdin().read_line(&mut input).expect(
-                "failed to get some input",
-            );
-            if input == "\n" {
-                child.kill().expect("was no afplay?");
-                child.wait().unwrap();
-            }
+        while child.try_wait().ok().map(|x| !x.is_some()).unwrap_or(true) {
+            let _ = rx.try_recv().map(|_| {
+                let _ = child.kill();
+                child.wait().expect("failed to kill afplay");
+            });
         }
     }
 
